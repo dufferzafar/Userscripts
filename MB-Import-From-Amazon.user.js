a00999f7f8351c0f7b0d92d8a22f2899a616d3df
Gore <marc@tempelmeier.org>
2015-09-22 13:43:26 +0200

    tidied up

--- MB-Import-From-Amazon.user.js
+++ MB-Import-From-Amazon.user.js
@@ -1,12 +1,13 @@
 ﻿// ==UserScript==
-// @id             MusicBrainz-Import-from-Amazon
-// @name           MusicBrainz: Import from Amazon
-// @version        0.9
-// @namespace      mb_import_from_amazon
-// @author         dufferZafar & Gore
-// @description    Import releases from Amazon
-// @include        *://www.amazon.*
-// @run-at         document-end
+// @id              MusicBrainz-Import-from-Amazon
+// @name            MusicBrainz: Import from Amazon
+// @namespace       mb_import_from_amazon
+// @include         *://www.amazon.*
+// @version         0.9
+// @grant           none
+// @author          dufferZafar & Gore
+// @description     Import releases from Amazon
+// @run-at          document-end
 // ==/UserScript==
 
 // configuration
@@ -15,9 +16,8 @@ var monthsDe = {"Januar": 1, "Februar": 2, "März": 3, "April": 4, "Mai": 5, "Ju
 var monthsCom = {"January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
 var regexReleaseDate = /Audio CD  \((.*)\)/;
 var regexReleaseLabel = /Label: (.*)/;
-alert("asd");
-switch (navigationItem)
-{
+
+switch (navigationItem) {
     // amazon.com
     case "CDs & Vinyl":
         var months = monthsCom;
@@ -68,155 +68,126 @@ switch (navigationItem)
         return;
 }
 
+var css = document.createElement("style");
+
+css.type = "text/css";
+css.innerHTML = "#mbForm {border: 1px solid #DDDDDD; border-radius: 4px; padding: 10px 14px 10px 14px;}";
+css.innerHTML += "#mbSubmit {border: 1px solid #6DAEE1; border-radius: 3px; cursor: pointer; padding: 4px 0px 4px 0px; margin: 5px 0px 0px 0px; width: 100%; background: #C9E1F4;} .mbSubmit:hover {background: #B2D3ED}";
+
+document.body.appendChild(css);
+
 var form = document.createElement("form");
+
+form.id = "mbForm";
 form.method="post";
 form.target = "_blank";
 form.action = "https://musicbrainz.org/release/add";
 form.acceptCharset = "UTF-8";
 
-var btnCSS = document.createElement("style");
-btnCSS.type = "text/css";
-btnCSS.innerHTML = ".mbBtn {border: 1px solid #6DAEE1; cursor: pointer; border-radius: 4px; padding: 10px 15px; margin-bottom: -10x; width: 100%; background: #C9E1F4;} .mbBtn:hover {background: #B2D3ED}"
-document.body.appendChild(btnCSS);
-
 var formSelectPrimaryType = document.createElement("select");
 formSelectPrimaryType.name = "type";
 
-var formOption = document.createElement("option");
-formOption.text = "Primary Type";
-formSelectPrimaryType.add(formOption);
-
-formOption = document.createElement("option");
-formOption.text = "Album";
-formSelectPrimaryType.add(formOption);
-
-formOption = document.createElement("option");
-formOption.text = "Single";
-formSelectPrimaryType.add(formOption);
-
-formOption = document.createElement("option");
-formOption.text = "EP";
-formSelectPrimaryType.add(formOption);
-
-formOption = document.createElement("option");
-formOption.text = "Broadcast";
-formSelectPrimaryType.add(formOption);
-
-formOption = document.createElement("option");
-formOption.text = "Other";
-formSelectPrimaryType.add(formOption);
+createSelectOption(formSelectPrimaryType, "Primary Type", "");
+createSelectOption(formSelectPrimaryType, "Album", "Album");
+createSelectOption(formSelectPrimaryType, "Single", "Single");
+createSelectOption(formSelectPrimaryType, "EP", "EP");
+createSelectOption(formSelectPrimaryType, "Broadcast", "Broadcast");
+createSelectOption(formSelectPrimaryType, "Other", "Other");
 
 form.appendChild(formSelectPrimaryType);
 
-form.appendChild(formSelectSecondaryType);
-
-var formSubmit = document.createElement("input");
-formSubmit.type = "submit";
-formSubmit.value = "Add release to MusicBrainz";
-formSubmit.classList.add("mbBtn");
-form.appendChild(formSubmit);
-
-switch (category)
-{
+switch (category) {
     case "cd":
         document.getElementById('tellAFriendBox_feature_div').appendChild(form);
 
         // Title of the Album
         // Todo: Use regex to extract ONLY album title
-        addInputToForm(form, "name", document.getElementById('productTitle').textContent);
+        createInput(form, "hidden", "name", document.getElementById('productTitle').textContent);
 
         // Album Artist (Composer)
         // Todo: Loop over <a> tags to find ALL composers
         var albumArtists = document.getElementsByClassName('author');
         var albumArtist = albumArtists[0].getElementsByTagName('a');
-        addInputToForm(form, "artist_credit.names.0.name", albumArtist[0].textContent);
+        createInput(form, "hidden", "artist_credit.names.0.name", albumArtist[0].textContent);
 
         // Date and Label
         var detailsTab = document.getElementById('productDetailsTable');
         var detailsList = detailsTab.getElementsByTagName('li');
         
         var match = null;
+        var medium = 0;
+        var track = 0;
 
-        for (var i = 0; i < detailsList.length; i++)
-        {   
+        for (var i = 0; i < detailsList.length; i++) {   
             match = regexReleaseDate.exec(detailsList[i].textContent);
             
-            if (match)
-            {
-                switch (domain)
-                {
+            if (match) {
+                switch (domain) {
                     case "de":
                         var splittedDate = match[1].split(" ");
 
-                        addInputToForm(form, "events.0.date.day", splittedDate[0].replace(/\./, ""));
-                        addInputToForm(form, "events.0.date.month", months[splittedDate[1]]);
-                        addInputToForm(form, "events.0.date.year", splittedDate[2]);
+                        createInput(form, "hidden", "events.0.date.day", splittedDate[0].replace(/\./, ""));
+                        createInput(form, "hidden", "events.0.date.month", months[splittedDate[1]]);
+                        createInput(form, "hidden", "events.0.date.year", splittedDate[2]);
                         break;
 
                     case "com":
                         var splittedDate = match[1].split(" ");
 
-                        addInputToForm(form, "events.0.date.day", splittedDate[1].replace(/,/, ""));
-                        addInputToForm(form, "events.0.date.month", months[splittedDate[0]]);
-                        addInputToForm(form, "events.0.date.year", splittedDate[2]);
+                        createInput(form, "hidden", "events.0.date.day", splittedDate[1].replace(/,/, ""));
+                        createInput(form, "hidden", "events.0.date.month", months[splittedDate[0]]);
+                        createInput(form, "hidden", "events.0.date.year", splittedDate[2]);
                         break;
                 }
             }
 
             match = regexReleaseLabel.exec(detailsList[i].textContent);
 
-            if (match)
-            {
-                addInputToForm(form, "labels.0.name", match[1]);
+            if (match) {
+                createInput(form, "hidden", "labels.0.name", match[1]);
             }
         }
 
-        var medium = 0;
-        var track = 0;
-        addInputToForm(form, "mediums.0.format", "CD");
+        createInput(form, "hidden", "mediums.0.format", "CD");
 
         // Amazon has more than one track listing...
-        if (document.getElementById("dmusic_tracklist_content")) 
-        {
+        if (document.getElementById("dmusic_tracklist_content")) {
             var tracklist = document.getElementById("dmusic_tracklist_content").getElementsByTagName("tr");
 
-            for (var i = 1; i < tracklist.length; i++)
-            {
+            for (var i = 1; i < tracklist.length; i++) {
                 if (tracklist[i].id == "dmusic_tracklist_player_disc_" + (medium + 2))
                 {
                     medium++;
                     track = 0;
-                    addInputToForm(form, "mediums." + medium + ".format", "CD");
+                    createInput(form, "hidden", "mediums." + medium + ".format", "CD");
                     continue;
                 }
 
                 var trackDetails = tracklist[i].getElementsByTagName("td");
                             
-                addInputToForm(form, "mediums." + medium + ".track." + track + ".number", trackDetails[0].getElementsByClassName("TrackNumber")[0].textContent);
-                addInputToForm(form, "mediums." + medium + ".track." + track + ".name", trackDetails[1].getElementsByClassName("TitleLink")[0].textContent);
-                addInputToForm(form, "mediums." + medium + ".track." + track + ".length", trackDetails[2].getElementsByTagName("span")[0].textContent.trim());
+                createInput(form, "hidden", "mediums." + medium + ".track." + track + ".number", trackDetails[0].getElementsByClassName("TrackNumber")[0].textContent);
+                createInput(form, "hidden", "mediums." + medium + ".track." + track + ".name", trackDetails[1].getElementsByClassName("TitleLink")[0].textContent);
+                createInput(form, "hidden", "mediums." + medium + ".track." + track + ".length", trackDetails[2].getElementsByTagName("span")[0].textContent.trim());
 
                 track++;
             }
         }
-        else if (document.getElementById("dmusic_tracklist_player"))
-        {
+        else if (document.getElementById("dmusic_tracklist_player")) {
             var tracklist = document.getElementById("dmusic_tracklist_player").getElementsByClassName("a-row");
 
-            for (var i = 1; i < tracklist.length; i++)
-            {
+            for (var i = 1; i < tracklist.length; i++) {
                 if (tracklist[i].textContent.trim() == disc + ": " + (medium + 2))
                 {
                     medium++;
                     track = 0;
-                    addInputToForm(form, "mediums." + medium + ".format", "CD");
+                    createInput(form, "hidden", "mediums." + medium + ".format", "CD");
                     continue;
                 }
 
                 var trackDetails = tracklist[i].textContent.split(". ");
 
-                addInputToForm(form, "mediums." + medium + ".track." + track + ".number", trackDetails[0].trim());
-                addInputToForm(form, "mediums." + medium + ".track." + track + ".name", trackDetails[1].trim());
+                createInput(form, "hidden", "mediums." + medium + ".track." + track + ".number", trackDetails[0].trim());
+                createInput(form, "hidden", "mediums." + medium + ".track." + track + ".name", trackDetails[1].trim());
             
                 track++;
             }
@@ -224,18 +195,42 @@ switch (category)
         break;
 }
 
-addInputToForm(form, "status", "official");
-addInputToForm(form, "edit_note", "Release added using the MB-Import-From-Amazon userscript from page: " + document.location.href);
-addInputToForm(form, "urls.0.url", document.location.href);
-addInputToForm(form, "urls.0.link_type", "77");
+createInput(form, "hidden", "status", "official");
+createInput(form, "hidden", "edit_note", "Release added using the MB-Import-From-Amazon userscript from page: " + document.location.href);
+createInput(form, "hidden", "urls.0.url", document.location.href);
+createInput(form, "hidden", "urls.0.link_type", "77");
 
-function addInputToForm(form, name, value) 
+createInput(form, "submit", "", "Add Release to MusicBrainz", "mbSubmit");
+
+function createInput(form, type, name, value, id, css) 
 {
     var input = document.createElement("input");
 
-    input.type = "hidden";
-    input.name = name;
+    if (id) {
+        input.id = id;
+    }
+
+    input.type = type;
+
+    if (name) {
+        input.name = name;
+    }
+
+    if (css) {
+        input.classList.add(css);
+    }
+    
     input.value = value;
 
     form.appendChild(input);
+}
+
+function createSelectOption(select, text, value)
+{
+    var selectOption = document.createElement("option");
+
+    selectOption.text = text;
+    selectOption.value = value;
+
+    select.add(selectOption);
 }
